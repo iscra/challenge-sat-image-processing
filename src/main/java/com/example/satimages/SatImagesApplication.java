a9d07c78fc28858e96b03ae12b63de55dc77819d
Adrian Halamoda <adrian.halamoda@gmail.com>
2019-02-16 10:52:40 +0100

    generated Spring Boot application

---
+++ src/main/java/com/example/satimages/SatImagesApplication.java
@@ -0,0 +1,14 @@
+package com.example.satimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatImagesApplication.class, args);
+	}
+
+}
+
